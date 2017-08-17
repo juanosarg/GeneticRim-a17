5a65be735c837860957f0ab1012384a4f2b44f69
sargbjornson <sarg.auxiliar@gmail.com>
2017-08-17 17:15:23 +0200

    Initial commit

---
+++ Source/NewHatcher/NewHatcher/CompProperties_Incubator.cs
@@ -0,0 +1,17 @@
+﻿using Verse;
+
+namespace NewHatcher
+{
+    public class CompProperties_Incubator : CompProperties
+    {
+        public float hatcherDaystoHatch = 1f;
+
+        public PawnKindDef hatcherPawn;
+
+        public CompProperties_Incubator()
+        {
+            //Messages.Message("Patataaa", MessageSound.Standard);
+            this.compClass = typeof(CompIncubator);
+        }
+    }
+}
